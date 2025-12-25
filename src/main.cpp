01a8fe34198a347ac15c772dd2f01bc855b430a6
mamutrahal269 <pidd386@outlook.com>
2025-12-25 10:50:57 +0700

    -

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,114 @@
 #include <boost/process.hpp>
 #include <chrono>
 #include <curl/curl.h>
+#include <random>
 
 using namespace boost;
 std::mutex stdout_mtx;
 
-int main() {
+int main(int argc, char* argv[]) {
+	std::vector<std::string> test_urls = {
+		"https://www.facebook.com",
+		"https://www.instagram.com",
+		"https://twitter.com",
+		"https://www.snapchat.com",
+		"https://www.reddit.com",
+		"https://www.patreon.com",
+		"https://www.linkedin.com",
+		"https://www.soundcloud.com",
+		"https://www.tiktok.com",
+		"https://signal.org",
+		"https://www.viber.com",
+		"https://www.whatsapp.com",
+		"https://www.apple.com/facetime",
+		"https://www.telegram.org",
+		"https://www.roblox.com",
+		"https://www.rutracker.org",
+		"https://www.envato.com",
+		"https://www.metacritic.com",
+		"https://www.tidal.com",
+		"https://www.pinterest.com",
+		"https://www.bbc.com",
+		"https://www.cnn.com",
+		"https://www.nytimes.com",
+		"https://www.washingtonpost.com",
+		"https://www.theguardian.com",
+		"https://www.euronews.com",
+		"https://www.radiofreeeurope.org",
+		"https://www.dw.com",
+		"https://www.repubblica.it",
+		"https://meduza.io",
+		"https://themoscowtimes.com",
+		"https://openrussia.org",
+		"https://imrussia.org",
+		"https://www.protonvpn.com",
+		"https://www.nordvpn.com",
+		"https://www.expressvpn.com",
+		"https://www.vpnservice.com",
+		"https://www.google.com",
+		"https://www.youtube.com",
+		"https://www.github.com",
+		"https://www.stackoverflow.com",
+		"https://vk.com",
+		"https://zona.media",
+		"https://baltnews.com",
+		"https://discord.com",
+		"https://www.chess.com",
+		"https://www.quora.com",
+		"https://www.archive.org",
+		"https://www.fbi.gov",
+		"https://www.cia.gov",
+		"https://www.npr.org",
+		"https://www.amnesty.org",
+		"https://www.hrw.org",
+		"https://www.theintercept.com",
+		"https://www.euronews.com",
+		"https://www.voanews.com",
+		"https://www.clarin.com",
+		"https://www.rai.it",
+		"https://www.nytimes.com",
+		"https://www.washingtonexaminer.com",
+		"https://www.bellingcat.com",
+		"https://www.tjournal.com",
+		"https://www.kitabook.net",
+		"https://www.freedomletters.org",
+		"https://www.goodreads.com",
+		"https://www.ruslania.com",
+		"https://www.bookamaro.com",
+		"https://www.nuntiare.org",
+		"https://www.museumoflgbthistory.org",
+		"https://www.truehistory.org",
+		"https://www.spotify.com",
+		"https://www.pornhub.com",
+		"https://www.xhamster.com",
+		"https://www.redtube.com",
+		"https://www.youjizz.com",
+		"https://www.xnxx.com",
+		"https://www.ero‑xnxx.com",
+		"https://www.booking.com",
+		"https://www.airbnb.com",
+		"https://www.tripadvisor.com",
+		"https://www.booking.com",
+		"https://www.expedia.com",
+		"https://www.hotels.com",
+		"https://www.skyscanner.net",
+		"https://www.discord.gg"
+	};
+
+
+	const size_t NTHREADS = std::stoi(argv[1]);
 	std::ifstream lists("lists");
 	std::ofstream out_conf("config.json", std::ios::trunc);
 	
-	std::vector<std::pair<std::string, uint16_t>> configs[4];
+	std::vector<std::pair<std::string, uint16_t>> configs[NTHREADS];
 	
-	json::object results[4], final;
+	json::object results[NTHREADS], final;
 	std::string line;
 	uint32_t start_port = 10809, port = start_port;
 	size_t idx = 0;
 
 	while(getline(lists, line) && port <= 0xFFFF)
-		configs[idx++ % 4].push_back({line, static_cast<uint16_t>(port++)});
+		configs[idx++ % NTHREADS].push_back({line, static_cast<uint16_t>(port++)});
 	
 	/* initialize results and final with default values */
 	final["inbounds"] = json::array{};
@@ -35,11 +126,11 @@ int main() {
 		{"domainStrategy", "AsIs"},
 		{"rules", json::array{}}
 	};
-	for(int i = 0; i < 4; ++i)
+	for(int i = 0; i < NTHREADS; ++i)
 		results[i] = final;
-	std::thread threads[4];
+	std::thread threads[NTHREADS];
 
-	for(int i = 0; i < 4; ++i) {
+	for(int i = 0; i < NTHREADS; ++i) {
 		threads[i] = std::thread([start_port, i, &configs, &results] {
 			try {
 				results[i] = configgen(configs[i], start_port);
@@ -53,11 +144,11 @@ int main() {
 		});
 	}
 
-	for(int i = 0; i < 4; ++i)
+	for(int i = 0; i < NTHREADS; ++i)
 		threads[i].join();
 	
 	/* merge */
-	for(int i = 0; i < 4; ++i) {
+	for(int i = 0; i < NTHREADS; ++i) {
 		/* inbounds */
 		for(const auto& val : results[i]["inbounds"].as_array())
 			final["inbounds"].as_array().push_back(val);
@@ -82,14 +173,18 @@ int main() {
 	
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	
-	for(int i = 0; i < 4; ++i) {
-		threads[i] = std::thread([&configs, i] {
+	for(int i = 0; i < NTHREADS; ++i) {
+		threads[i] = std::thread([&test_urls, &configs, i] {
 			std::ostringstream stdout_buf, stderr_buf;
 			for(const auto& [conf, port] : configs[i]) {
-				const auto info = httpcheck(port, "https://www.youtube.com", 5, std::cerr);
+				std::random_device rd;
+				std::mt19937 gen(rd());
+				std::uniform_int_distribution<size_t> dist(0, test_urls.size() - 1);
+				const auto& rand_url = test_urls[dist(gen)];
+				const auto info = httpcheck(port, rand_url, 5, stderr_buf);
 				
 				if(info.http_code != 0) {
-					const auto metric = geometric_with_proxy(port, 5, std::cerr);
+					const auto metric = geometric_with_proxy(port, 5, stderr_buf);
 					stdout_buf << "\nConfiguration: " << conf << "\n";
 					stdout_buf << "====================ipinfo.io/json====================\n";
 					stdout_buf << "IP: " << metric.ip << "\n";
@@ -101,6 +196,7 @@ int main() {
 					stdout_buf << "LOC: " << metric.loc << "\n";
 					stdout_buf << "Postal: " << metric.postal << "\n";
 					stdout_buf << "======================================================\n";
+					stdout_buf << "URL: " << rand_url << "\n";
 					stdout_buf << "HTTP Code: " << info.http_code << "\n";
 					stdout_buf << "DNS lookup: " << info.t_dns*1000 << " ms\n";
 					stdout_buf << "Connect: " << info.t_connect*1000 << " ms\n";
@@ -117,7 +213,7 @@ int main() {
 			std::cerr << stderr_buf.str();
 		});
 	}
-	for(int i = 0; i < 4; ++i)
+	for(int i = 0; i < NTHREADS; ++i)
 		threads[i].join();
 	curl_global_cleanup();
 	return 0;
